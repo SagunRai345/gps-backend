ffed45f3351760410dfed77b9d753491daf07f78
SagunRai345 <raisagun333@gmail.com>
2025-05-13 07:52:34 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
+
+# To store GPS data
 gps_data = []
 
-@app.route('/gps_data', methods=['GET']) 
+@app.route('/')
+def home():
+    # Display the title and the current GPS data
+    return """
+    <html>
+        <head><title>GPS DATA LOGGER</title></head>
+        <body>
+            <h1>GPS DATA LOGGER</h1>
+            <h2>Current GPS Data:</h2>
+            <ul>
+                """ + ''.join([f"<li>Latitude: {data['lat']}, Longitude: {data['lng']}, Speed: {data['speed']} km/h</li>" for data in gps_data]) + """
+            </ul>
+            <p>If no data is received yet, it will appear here as soon as it's available.</p>
+        </body>
+    </html>
+    """
+
+@app.route('/gps_data', methods=['GET'])
 def gps_data_handler():
     lat = request.args.get('latitude')
     lng = request.args.get('longitude')
     speed = request.args.get('speed')
 
-    print(f"Received lat={lat}, lng={lng}, speed={speed}")  # <--- Add this for debugging
-
     if lat and lng and speed:
+        # Append new GPS data
         gps_data.append({'lat': lat, 'lng': lng, 'speed': speed})
         return "Data stored", 200
-    return "Missing data", 400
+    else:
+        # Return a response indicating missing data
+        return "Missing data", 400
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
-@app.route('/gps_data/view', methods=['GET'])
-def view_all_data():
-    return jsonify(gps_data)
